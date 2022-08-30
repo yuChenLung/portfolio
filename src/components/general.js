42ed4b42d6571fae53a81c44d2a52de3a023a796
yuChenLung <yuchen.lung1@gmail.com>
2022-08-29 20:42:47 -0400

    small changes to tab title and image

--- src/components/general.js
+++ src/components/general.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../index.css';
 import './general.css';
 import { NavLink } from 'react-router-dom';
+import '../assets/images/auto-cashier.png';
 
 const Feature = (props) => {
     return(
@@ -14,7 +15,8 @@ const Feature = (props) => {
                     <NavLink to='/feature-proj'>See More -{'>'}</NavLink>
                 </div>
             </div>
-            <img id='feature-img' src={require('../assets/images/auto-cashier.png')} alt='Featured Image'/>
+            <img id='feature-img' src={require('./portfolio/auto-cashier.png')} alt='Featured Image'/>
+            {/* https://stackoverflow.com/questions/51002481/images-not-loading-when-deploying-to-github-pages */}
         </div>
     );
 };
--- src/components/navbar/navbar-elements.js
+++ src/components/navbar/navbar-elements.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../../index.css';
 import './navbar.css';
 import { NavLink } from 'react-router-dom';
